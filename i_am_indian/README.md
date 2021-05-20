017813fbd4a472c6f6893d27a9de49bf916cbbd0
Yashasvi Bhatt <yashasvibhatt9@gmail.com>
2021-05-20 13:21:18 +0530

    Added I_am_Indian App

---
+++ i_am_indian/README.md
@@ -0,0 +1,9 @@
+# i_am_indian
+
+My first ever flutter project, made just for learning purpose.
+
+- I have made this project with the help of Udemy Course on Flutter Development taught by Angela Yu of appbrewery.com
+- This project is first of the 12 projects we will be making from this course.
+- The snapshot of the app looks like this:
+<br><br>
+![App Snapshot](images/snap.png)
---
+++ i_am_indian/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  double dimension = 200;
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green[400],
+        appBar: AppBar(
+          backgroundColor: Colors.orange[400],
+          title: Text(
+            'I am an Indian',
+            style: TextStyle(color: Colors.blue[800]),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            // "Column" Widget is used to add multiple widgets vertically with the help of "Expanded" Widget
+            children: <Widget>[
+              Expanded(
+                // Sub-Widget to provide a starting gap
+                child: Text(
+                  ' ',
+                ),
+              ),
+              Expanded(
+                // Sub-Widget to render Image from Internet
+                child: Image(
+                  image: NetworkImage(
+                      'https://st.depositphotos.com/1820591/1447/v/950/depositphotos_14478983-stock-illustration-creative-indian-flag-design.jpg'),
+                  height: dimension,
+                  width: dimension,
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Expanded(
+                // Sub-Widget to title the Image rendered Internet above
+                child: Text(
+                  'The Above Image is rendered from Internet',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              Expanded(
+                // Sub-Widget to render Image from Directory
+                child: Image(
+                  image: AssetImage('images/tricolor.jpg'),
+                  height: dimension,
+                  width: dimension,
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  'The Above Image is rendered from System\'s Directory',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
